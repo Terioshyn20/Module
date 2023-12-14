c9e20ca46f67ccf7a865d4602664e92c2b47704e
Terioshyn20 <153726432+Terioshyn20@users.noreply.github.com>
2023-12-14 13:47:30 +0200

    Add files via upload

---
+++ module/module/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.WriteLine("Введіть числа через кому:");
+        string input = Console.ReadLine();
+
+        List<int> numbers = input.Split(',').Select(int.Parse).ToList();
+
+        List<int> result = AddTenToEachNumber(numbers);
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(string.Join(", ", result));
+    }
+
+    static List<int> AddTenToEachNumber(List<int> inputNumbers)
+    {
+        return inputNumbers.Select(x => x + 10).ToList();
+    }
+}
